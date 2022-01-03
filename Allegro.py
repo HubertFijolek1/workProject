465e8f7f7d8891b39f8da86f81287d2feac686a3
Hubert Fijołek <hubertfijolek1@gmail.com>
2022-01-03 20:24:38 +0100

    adding task

--- Allegro.py
+++ Allegro.py
@@ -1,16 +1,27 @@
-from github import Github
+import requests
+import json
+def connection():
+    global username 
+    username = str(input("enter any github username "))
+    url = "https://api.github.com/users/{}/repos".format(username)
+    requests.get(url)
+    return requests.get(url)
+response = connection()
+while response.status_code == 404:
+        print("Invalid username. Please enter a valid name")
+        response = connection()
+if response.ok:
+    output = json.loads(response.text)
+    i = numberOfStar = 0
+    try:
+        while output[i]['full_name'] != None:
+            print(f'Repository name: {output[i]["full_name"]}, number of stars for this repository: {output[i]["stargazers_count"]}')
+            numberOfStar += output[i]["stargazers_count"]
+            i+=1
+    except IndexError:
+        pass
+    print("The total number of repositories is" + str(i))
+    print(f"The sum of the stars in all repositories is {numberOfStar} ")
 
-#My private token, please don't touch it ;)
-g = Github('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') 
-
-numberOfStars = 0
-for repo in g.get_user().get_repos():
-    repo=g.get_repo(f"HubertFijolek1/{repo.name}")
-    print("Repository name" + repo.name + "\nthe number of stars for this repository " + str(repo.stargazers_count))
-    numberOfStars+= repo.stargazers_count
-
-print("Total stars for all repositories" + str(numberOfStars))
     
-
-
-
+    
